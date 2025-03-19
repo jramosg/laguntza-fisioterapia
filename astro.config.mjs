444e2fe5d88a1ebf3460ec245e3b73667a825735
Jon <jonurnieta@gmail.com>
2025-03-19 20:27:41 +0100

    blog i18n support

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,18 @@ import mdx from '@astrojs/mdx';
 import icon from 'astro-icon';
 import lit from '@astrojs/lit';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://laguntza-fisioterapia.com/', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-	sitemap: true, // Generate sitemap (set to "false" to disable)
-	integrations: [sitemap(), mdx(), lit(), icon()], // Add renderers to the config
-	i18n: {
-		locales: ['es', 'eu'],
-		defaultLocale: 'es',
-	},
-	routing: {
-		prefixDefaultLocale: true,
-	},
+    site: 'https://laguntza-fisioterapia.com/', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+    sitemap: true, // Generate sitemap (set to "false" to disable)
+    integrations: [sitemap(), mdx(), lit(), icon(), partytown()], // Add renderers to the config
+    i18n: {
+        locales: ['es', 'eu'],
+        defaultLocale: 'es',
+    },
+    routing: {
+        prefixDefaultLocale: true,
+    },
 });
--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,13 @@
+import { Langs } from "@i18n/ui";
+
 export type Slug = `/${string}`;
+
+export interface Frontmatter {
+	title: string;
+	description: string;
+	authors: string[];
+	publishDate: string;
+	featuredImage: string;
+    excerpt: string;
+    lang: Langs
+}
