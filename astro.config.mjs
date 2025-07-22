e6729036692ed21c4a6c4e914930c32e5a5d2624
Jon <jonurnieta@gmail.com>
2025-07-22 07:58:46 +0200

    update dependencies and remove unused lit dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 import icon from 'astro-icon';
-import lit from '@astrojs/lit';
 
 import partytown from '@astrojs/partytown';
 
@@ -10,7 +9,7 @@ import partytown from '@astrojs/partytown';
 export default defineConfig({
 	site: 'https://laguntzafisioterapia.com/',
 	sitemap: true,
-	integrations: [sitemap(), mdx(), lit(), icon(), partytown()], // Add renderers to the config
+	integrations: [sitemap(), mdx(), icon(), partytown()], // Add renderers to the config
 	i18n: {
 		locales: ['es', 'eu'],
 		defaultLocale: 'es',
